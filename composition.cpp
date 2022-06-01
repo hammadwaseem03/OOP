f6bfbea8fcf8606527726a3934c8c516ca8f8d69
hammadwaseem03 <78177798+hammadwaseem03@users.noreply.github.com>
2022-06-01 17:36:32 +0500

    Update composition.cpp

--- composition.cpp
+++ composition.cpp
@@ -6,14 +6,14 @@ public:
         cout << "Constructor of Birthday class" <<endl;
     }
     Birthday(int m,int d,int y){
-    month=m;
-    year=y;
-    day=d;
-    cout << "Constructor of Birthday class" << endl; 
+        month=m;
+        year=y;
+        day=d;
+        cout << "Constructor of Birthday class" << endl; 
     }
     
     ~Birthday(){
-        std::cout << "Destructor of Birthday class" <<endl;
+        cout << "Destructor of Birthday class" <<endl;
     }
     void printDate(){
         cout << month << "/" << day << "/" << year <<endl;
@@ -28,27 +28,28 @@ class Person{
 public: Person(){
 
 }
-Person(string ,const Birthday&){
-
-}
-
-~Person(){
-    cout << "Destructor of Person class" << endl;
+Person(string name ,const Birthday& dateofBirth){
+    this->name=name;
+    this->dateofBirth=dateofBirth;
 }
 void printInfo(){
     cout << name << " was born on ";
-dateofBirth.printDate();
+    dateofBirth.printDate();
 }
-private:
-string name;
-Birthday dateofBirth;
-};
-int main(){
- Birthday b(2, 2, 2000);
- Person p1("Ali",b);
- p1.printInfo();
-return 0;
+~Person(){
+    cout << "Destructor of Person class" << endl;
 }
 
+private:
+    string name;
+    Birthday dateofBirth;
+};
+int main(){
+    Birthday b(2, 3, 2002);
+    Person p1("Ali",b);
+    p1.printInfo();
+    return 0;
+}
+ 
 
 
